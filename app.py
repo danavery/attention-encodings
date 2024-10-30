9f44f52420a62d2f406749682f39233a8e36804a
Dan Avery <dan@danavery.com>
2024-10-30 14:28:53 -0700

    refactor to AttentionAnalyzer

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ debug = False
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
-class RobertaTransformer:
+class TransformerManager:
     def __init__(self, model_name="roberta-base"):
         self.model_name = model_name
         self._load_model(model_name)
@@ -60,13 +60,6 @@ class RobertaTransformer:
             )
             print(f"{self.position_adjusted_vocab[selected_token_idx][:5]=}")
 
-    def project_to_value_by_head(self, embeddings, value_weights):
-        # compute value encodings for all words
-        x = torch.einsum("nd,df->nf", embeddings, value_weights)
-        # reshape into individual head value encodings
-        x_heads = x.view(x.size(0), self.config.num_attention_heads, self.head_size)
-        return x_heads
-
     def get_layer_value_weights(self, selected_layer):
         return self.model.encoder.layer[selected_layer].attention.self.value.weight
 
@@ -74,27 +67,56 @@ class RobertaTransformer:
         attentions = outputs.attentions[selected_layer]
         return attentions
 
+
+class AttentionAnalyzer:
+    def __init__(self, transformer, k=15):
+        self.transformer = transformer
+        self.k = k
+
+    def get_tokens_for_text(self, text):
+        inputs = self.transformer.tokenizer(text, return_tensors="pt", truncation=True)
+        input_ids = inputs["input_ids"]
+        tokens = self.transformer.tokenizer.convert_ids_to_tokens(input_ids[0])
+        token_list = [[token] for token in tokens]
+        return token_list
+
+    def get_distances(
+        self,post_attention_encoding, comparison_encodings, distance_type="Euclidean"
+    ):
+        """
+        Compute the distances between a post-attention encoding and a set of comparison encodings.
+        """
+        if distance_type == "Euclidean":
+            return F.pairwise_distance(post_attention_encoding, comparison_encodings)
+        else:
+            return 1 - F.cosine_similarity(
+                post_attention_encoding, comparison_encodings, dim=-1
+            )
+
     def get_all_layer_0_value_encodings(self, apply_positional_embeddings=True):
-        layer_0_value_weights = self.get_layer_value_weights(0)
+        layer_0_value_weights = self.transformer.get_layer_value_weights(0)
 
         if apply_positional_embeddings:
             layer0_value_encodings = self.project_to_value_by_head(
-                self.normalized_position_adjusted_vocab,
+                self.transformer.normalized_position_adjusted_vocab,
                 layer_0_value_weights,
             )
         else:
             layer0_value_encodings = self.project_to_value_by_head(
-                self.normalized_vocab_embeddings,
+                self.transformer.normalized_vocab_embeddings,
                 layer_0_value_weights,
             )
         return layer0_value_encodings
 
-    def tokens_for_text(self, text):
-        inputs = self.tokenizer(text, return_tensors="pt", truncation=True)
-        input_ids = inputs["input_ids"]
-        tokens = self.tokenizer.convert_ids_to_tokens(input_ids[0])
-        token_list = [[token] for token in tokens]
-        return token_list
+
+
+    def project_to_value_by_head(self, embeddings, value_weights):
+        ''''''
+        x = torch.einsum("nd,df->nf", embeddings, value_weights)
+        # reshape into individual head value encodings
+        x_heads = x.view(x.size(0), self.transformer.config.num_attention_heads, self.transformer.head_size)
+        return x_heads
+
 
     def compute_post_attention_values(self, selected_layer, outputs):
         """
@@ -103,7 +125,7 @@ class RobertaTransformer:
         """
         selected_layer = int(selected_layer)
         # get weights for selected_layer
-        selected_layer_value_weights = self.get_layer_value_weights(
+        selected_layer_value_weights = self.transformer.get_layer_value_weights(
             selected_layer
         )
 
@@ -120,15 +142,15 @@ class RobertaTransformer:
 
         # get all attention weights for all heads, single layer, single token
         # <batch_size, num_heads, seq_len, seq_len>
-        layer_attention_weights = self.get_layer_attention_weights(outputs, selected_layer)
+        layer_attention_weights = self.transformer.get_layer_attention_weights(outputs, selected_layer)
 
         # split value encoding into per-head encodings
         # <batch_size, seq_len, num_heads, hidden_size/num_heads>
         encodings_per_head = value_encodings.view(
             value_encodings.size(0),
             value_encodings.size(1),
-            self.config.num_attention_heads,
-            self.head_size,
+            self.transformer.config.num_attention_heads,
+            self.transformer.head_size,
         )
 
         # <batch_size, seq_len, num_heads, hidden_size/num_heads>
@@ -138,53 +160,29 @@ class RobertaTransformer:
 
         return post_attn_layer_encodings.squeeze(0)
 
-
-def get_distances(
-    post_attention_encoding, comparison_encodings, distance_type="Euclidean"
-):
-    """
-    Compute the distances between a post-attention encoding and a set of comparison encodings.
-    """
-    if distance_type == "Euclidean":
-        return F.pairwise_distance(post_attention_encoding, comparison_encodings)
-    else:
-        return 1 - F.cosine_similarity(
-            post_attention_encoding, comparison_encodings, dim=-1
+    def get_closest(
+        self,
+        k,
+        transformer,
+        input_ids,
+        post_attention_encoding,
+        comparison_encodings,
+        distance_type="Euclidean",
+    ):
+        encoding_distances = self.get_distances(
+            post_attention_encoding, comparison_encodings, distance_type
         )
+        distances, encodings = torch.topk(encoding_distances, k, largest=False)
 
+        if input_ids is not None:  # the indexes are into input_ids instead of encodings
+            encodings_str = transformer.tokenizer.convert_ids_to_tokens(
+                input_ids[0, encodings].tolist()
+            )
+        else:
+            encodings_str = transformer.tokenizer.convert_ids_to_tokens(encodings.tolist())
+        distances = ["{0:.3f}".format(distance.item()) for distance in distances]
 
-def get_closest(
-    k,
-    transformer,
-    input_ids,
-    post_attention_encoding,
-    comparison_encodings,
-    distance_type="Euclidean",
-):
-    encoding_distances = get_distances(
-        post_attention_encoding, comparison_encodings, distance_type
-    )
-    distances, encodings = torch.topk(encoding_distances, k, largest=False)
-
-    if input_ids is not None:  # the indexes are into input_ids instead of encodings
-        encodings_str = transformer.tokenizer.convert_ids_to_tokens(
-            input_ids[0, encodings].tolist()
-        )
-    else:
-        encodings_str = transformer.tokenizer.convert_ids_to_tokens(encodings.tolist())
-    distances = ["{0:.3f}".format(distance.item()) for distance in distances]
-
-    return list(zip(encodings_str, distances))
-
-
-class App:
-    def __init__(self):
-        self.transformer = RobertaTransformer()
-        self.k = 15
-
-    def update_tokens(self, text):
-        tokens = self.transformer.tokens_for_text(text)
-        return gr.update(samples=tokens, components=["text"])
+        return list(zip(encodings_str, distances))
 
     def closest_to_all_values(
         self,
@@ -214,7 +212,7 @@ class App:
         decoded_tokens = f"{self.transformer.tokenizer.convert_ids_to_tokens(input_ids[0, selected_token].item())} ({input_ids[0, selected_token]})"
 
         # get all value encodings for all tokens at layer 0
-        layer_0_value_encodings = self.transformer.get_all_layer_0_value_encodings(
+        layer_0_value_encodings = self.get_all_layer_0_value_encodings(
             apply_positional_embeddings=apply_positional_embeddings
         )
 
@@ -223,13 +221,13 @@ class App:
 
 
         # get the post-attention encodings for all tokens at the selected layer
-        post_attention_encodings = self.transformer.compute_post_attention_values(
+        post_attention_encodings = self.compute_post_attention_values(
             selected_layer, outputs
         )
         closest_df = []
         closest_outputs_df = []
         for head in range(self.transformer.config.num_attention_heads):
-            closest_for_head = get_closest(
+            closest_for_head = self.get_closest(
                 self.k,
                 self.transformer,
                 None,
@@ -240,7 +238,7 @@ class App:
             closest_for_head = [f"{token} ({dist})" for token, dist in closest_for_head]
             closest_df.append(pd.DataFrame(closest_for_head, columns=[f"head {head}"]))
 
-            closest_outputs_for_head = get_closest(
+            closest_outputs_for_head = self.get_closest(
                 len(input_ids[0]),
                 self.transformer,
                 input_ids,
@@ -259,6 +257,11 @@ class App:
         closest_outputs_df = pd.concat(closest_outputs_df, axis=1)
         return (decoded_tokens, closest_df, closest_outputs_df)
 
+class AttentionAnalyzerUI:
+    def __init__(self, analyzer: AttentionAnalyzer):
+        self.analyzer = analyzer
+        self.k = analyzer.k
+
     def get_intro_markdown(self):
         with open("./README.md", "r") as file:
             intro_markdown = file.read()
@@ -266,6 +269,10 @@ class App:
             if position != -1:
                 intro_markdown = intro_markdown[position:]
         return intro_markdown
+
+    def update_token_display(self, text):
+        tokens = self.analyzer.get_tokens_for_text(text)
+        return gr.update(samples=tokens, components=["text"])
 
     def launch(self):
         intro_markdown = self.get_intro_markdown()
@@ -311,7 +318,7 @@ class App:
                 )
                 tokens = gr.Dataset(
                     components=["text"],
-                    samples=self.transformer.tokens_for_text(initial_text),
+                    samples=self.analyzer.get_tokens_for_text(initial_text),
                     type="index",
                     label="Select a token from here to see its closest tokens after attention processing. Click one to see its neighbors below.",
                     scale=5,
@@ -339,33 +346,33 @@ class App:
                 )
 
             tokens.click(
-                fn=self.closest_to_all_values,
+                fn=self.analyzer.closest_to_all_values,
                 inputs=[text, tokens, selected_layer, distance_type, use_positional],
                 outputs=[selected_token_str, combined_dataframe, intertoken_dataframe],
             )
             selected_layer.change(
-                fn=self.closest_to_all_values,
+                fn=self.analyzer.closest_to_all_values,
                 inputs=[text, tokens, selected_layer, distance_type, use_positional],
                 outputs=[selected_token_str, combined_dataframe, intertoken_dataframe],
             )
             distance_type.change(
-                fn=self.closest_to_all_values,
+                fn=self.analyzer.closest_to_all_values,
                 inputs=[text, tokens, selected_layer, distance_type, use_positional],
                 outputs=[selected_token_str, combined_dataframe, intertoken_dataframe],
             )
             use_positional.change(
-                fn=self.closest_to_all_values,
+                fn=self.analyzer.closest_to_all_values,
                 inputs=[text, tokens, selected_layer, distance_type, use_positional],
                 outputs=[selected_token_str, combined_dataframe, intertoken_dataframe],
             )
             demo.load(
-                fn=self.closest_to_all_values,
+                fn=self.analyzer.closest_to_all_values,
                 inputs=[text, tokens, selected_layer, distance_type, use_positional],
                 outputs=[selected_token_str, combined_dataframe, intertoken_dataframe],
             )
 
-            show_tokens.click(self.update_tokens, inputs=[text], outputs=[tokens])
-            text.submit(self.update_tokens, inputs=[text], outputs=[tokens])
+            show_tokens.click(self.update_token_display, inputs=[text], outputs=[tokens])
+            text.submit(self.update_token_display, inputs=[text], outputs=[tokens])
 
             gr.Markdown(intro_markdown)
             try:
@@ -374,5 +381,15 @@ class App:
                 combined_dataframe.change(js=custom_js)
         demo.launch(server_name="0.0.0.0")
 
+class App:
+    def __init__(self):
+        transformer = TransformerManager()
+        analyzer = AttentionAnalyzer(transformer)
+        self.ui = AttentionAnalyzerUI(analyzer)
 
-App().launch()
+    def launch(self):
+        self.ui.launch()
+
+if __name__ == "__main__":
+    App().launch()
+
